3f5a1978673c6fa9f3bc3095a21f2ec2252dac6d
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-04-13 19:02:53 +0530

    Added new class for maintaining the Money. And did some re-factor by introduction the abstruction by the help of money class

---
+++ DDDInPractice.Logic/Money.cs
@@ -0,0 +1,59 @@
+﻿namespace DDDInPractice.Logic;
+
+// Which designing the snack machine class 
+// we found that all the notion is related to money
+// so we create abstraction for Money class
+public sealed class Money
+{
+    public int OneCentCount { get; set; }
+    public int TenCentCount { get; set; }
+    public int QuarterCount { get; set; }
+    public int OneDollerCount { get; set; }
+    public int FiveDollerCount { get; set; }
+    public int TwentyDollerCount { get; set; }
+
+    /// <summary>
+    /// Initializes money class using the constructor
+    /// </summary>
+    /// <param name="oneCentCount">The one cent count.</param>
+    /// <param name="tenCentCount">The ten cent count.</param>
+    /// <param name="quarterCount">The quarter count.</param>
+    /// <param name="oneDollerCount">The one doller count.</param>
+    /// <param name="fiveDollerCount">The five doller count.</param>
+    /// <param name="twentyDollerCount">The twenty doller count.</param>
+    public Money(int oneCentCount,
+                 int tenCentCount,
+                 int quarterCount,
+                 int oneDollerCount,
+                 int fiveDollerCount,
+                 int twentyDollerCount)
+    {
+        // here we are tracking what money is the user is using in the transaction
+        OneCentCount = oneCentCount;
+        TenCentCount = tenCentCount;
+        QuarterCount = quarterCount;
+        OneDollerCount = oneDollerCount;
+        FiveDollerCount = fiveDollerCount;
+        TwentyDollerCount = twentyDollerCount;
+    }
+
+    /// <summary>
+    /// Implements the operator +.
+    /// </summary>
+    /// <param name="money1">The money1.</param>
+    /// <param name="money2">The money2.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static Money operator +(Money money1, Money money2)
+    {
+        Money sum = new Money(
+            money1.OneCentCount + money2.OneCentCount,
+            money1.TenCentCount + money2.TenCentCount,
+            money1.QuarterCount + money2.QuarterCount,
+            money1.OneDollerCount + money2.OneDollerCount,
+            money1.FiveDollerCount + money2.FiveDollerCount,
+            money1.TwentyDollerCount + money2.TwentyDollerCount);
+        return sum;
+    }
+}
--- DDDInPractice.Logic/SnackMachine.cs
+++ DDDInPractice.Logic/SnackMachine.cs
@@ -3,12 +3,8 @@
 // creating a sealed class so that this class can not be inherited
 public sealed class SnackMachine
 {
-    public int OneCentCount { get; private set; }
-    public int TenCentCount { get; private set; }
-    public int QuarterCount { get; private set; }
-    public int OneDollerCount { get; private set; }
-    public int FiveDollerCount { get; private set; }
-    public int TwentyDollerCount { get; private set; }
+    public Money MoneyInside { get; private set; }
+    public Money MoneyInTransaction { get; private set; }
 
     // here we are adding separate properties
     // as we need to somehow distinguish the money is in the snack machine 
@@ -27,25 +23,11 @@ public sealed class SnackMachine
     /// Inserts the money to the snack machine
     /// </summary>
     /// <param name="oneCentCount">The one cent count.</param>
-    /// <param name="tenCentCount">The ten cent count.</param>
-    /// <param name="quarterCount">The quarter count.</param>
-    /// <param name="oneDollerCount">The one doller count.</param>
-    /// <param name="fiveDollerCount">The five doller count.</param>
-    /// <param name="twentityDollerCount">The twenty doller count.</param>
-    public void InsertMoney(int oneCentCount, 
-                        int tenCentCount, 
-                        int quarterCount, 
-                        int oneDollerCount, 
-                        int fiveDollerCount, 
-                        int twentyDollerCount)
+    
+    public void InsertMoney(Money money)
     {
         // here we are tracking what money is the user is using in the transaction
-        OneCentCountInTransaction += oneCentCount;
-        TenCentCountInTransaction += tenCentCount;
-        QuarterCountInTransaction += quarterCount;
-        OneDollerCountInTransaction += oneDollerCount;
-        FiveDollerCountInTransaction += fiveDollerCount;
-        TwentyDollerCountInTransaction += twentyDollerCount;
+        MoneyInTransaction += money;
     }
 
     /// <summary>
@@ -55,12 +37,7 @@ public sealed class SnackMachine
     {
         // when user will ask to return the money from the snack machine 
         // the all used transaction properties will be set to 0
-        OneCentCountInTransaction = 0;
-        TenCentCountInTransaction = 0;
-        QuarterCountInTransaction = 0;
-        OneDollerCountInTransaction = 0;
-        FiveDollerCountInTransaction = 0;
-        TwentyDollerCountInTransaction = 0;
+        //MoneyInTransaction = 0;
     }
 
     /// <summary>
@@ -71,19 +48,9 @@ public sealed class SnackMachine
         //if the user confirms to buy the snack 
         // we will be store the money to machine
         // using the transaction amount
-        OneCentCount += OneCentCountInTransaction;
-        TenCentCount += TenCentCountInTransaction;
-        QuarterCount += QuarterCountInTransaction;
-        OneCentCount += OneCentCountInTransaction;
-        FiveDollerCount += FiveDollerCountInTransaction;
-        TwentyDollerCount += TwentyDollerCountInTransaction;
+        MoneyInside += MoneyInTransaction;
 
         // after adding the transaction value we need to reset the transaction value
-        OneCentCountInTransaction = 0;
-        TenCentCountInTransaction = 0;
-        QuarterCountInTransaction = 0;
-        OneDollerCountInTransaction = 0;
-        FiveDollerCountInTransaction = 0;
-        TwentyDollerCountInTransaction = 0;
+        //MoneyInTransaction = 0;
     }
 }
